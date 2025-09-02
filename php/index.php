4e0e9d5c1f6817c484b8585ef751317f290b3af8
landoderi <deryaalylazuardi@gmail.com>
2025-09-02 08:23:33 +0700

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php $nama = "Derya";
+          $umur = 16;?>
+    <h1>Selamat datang, <?php echo "$nama"; ?>!</h1>
+
+    <p><h1>Umur Anda <?php echo "$umur"; ?>!</h1></p>
+</body>
+</html>
