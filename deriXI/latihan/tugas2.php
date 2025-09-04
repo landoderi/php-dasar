5194a0ed10ac3f62a7b57fd8af680d31c4a9bac2
landoderi <deryaalylazuardi@gmail.com>
2025-09-04 13:03:15 +0700

    Delete deriXI directory

--- deriXI/latihan/tugas2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$buah = ["Budi", "Ani", "Siti"];
-
-foreach ($buah as $b) {
-    echo "Nama: $b <br>";
-}
--- deriXI/materi/while.php
+++
@@ -1,9 +0,0 @@
-<?php
-$i = 1;
-while ($i <= 3) {
-    echo "nomor: $i <br>";
-    $i++;
-    if ($i == 3) {
-        break;
-    }
-}         
